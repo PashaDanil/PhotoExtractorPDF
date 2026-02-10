848d760f70e0d9a8ab407b2166e9f339c3a73f6f
BOOOO <peredoz224@gmail.com>
2026-02-10 13:23:47 +1000

    feat(inits): make empty init files, to mark directories as libraries

---
+++ apps/extractor/src/application/ports/__init__.py
@@ -0,0 +1,14 @@
+from .pdf_reader import PDFReader, AsyncDocumentHandle, PageRaster
+# from .image_detector import ImageDetector, Detection
+# from .image_cropper import ImageCropper
+# from .file_storage import FileStorage
+#
+__all__ = [
+    "PDFReader",
+    "AsyncDocumentHandle",
+    "PageRaster"
+    # "ImageDetector",
+    # "Detection",
+    # "ImageCropper",
+    # "FileStorage",
+]
