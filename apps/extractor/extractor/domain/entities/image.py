2470274fd84dcc4dbe9def7aed31b4983b5ba74f
paupaupau <94736048+peredz@users.noreply.github.com>
2026-02-16 14:23:44 +1000

    fix(image): remove confidence from Image domain entity

--- apps/extractor/extractor/domain/entities/image.py
+++ apps/extractor/extractor/domain/entities/image.py
@@ -13,13 +13,12 @@ class Image:
     id: UUID
     page_number: int
     bounding_box: BoundingBox
-    confidence: float
 
     def __post_init__(self):
         """Валидация"""
         if self.page_number < 0:
             raise ValueError(f"page_number должен быть >= 0, получено: {self.page_number}")
-        if (self.confidence < 0) or (self.confidence > 1):
+        if (self.bounding_box.confidence < 0) or (self.bounding_box.confidence > 1):
             raise ValueError(f"confidence должен быть от 0 до 1")
 
     @classmethod
@@ -34,7 +33,6 @@ class Image:
             id=uuid4(),
             page_number=page_number,
             bounding_box=bounding_box,
-            confidence=confidence,
         )
 
     def with_custom_bbox(self, padding: float) -> Self:
@@ -43,6 +41,4 @@ class Image:
             id=self.id,
             page_number=self.page_number,
             bounding_box=self.bounding_box.pad(padding),
-            confidence=self.confidence,
-            image_type=self.image_type
         )
